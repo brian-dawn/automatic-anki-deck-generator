5bbecd7c0d5fd785b9cdcf1d993ca6e8f9a520a2
Brian Dawn <brian.t.dawn@gmail.com>
2022-01-08 13:21:05 -0600

    Get voice downloading working.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import boto3
 from jamdict import Jamdict
 
 
+polly_client = boto3.Session(region_name="us-west-2").client("polly")
+
 jam = Jamdict()
 
 
@@ -13,37 +15,44 @@ def add_card(deck, model, word):
 
     japanese, english = found.text().split(" : ")
 
-
     print(f"{japanese} => {english}")
+    # Also Mizuki but doesn't support Neural voice I think.
+    response = polly_client.synthesize_speech(
+        VoiceId="Takumi", OutputFormat="mp3", Text=japanese, Engine="neural"
+    )
 
-    note = genanki.Note(
-            model=model,
-            fields=[japanese, english]
-            )
+    with open(f"{english}.mp3", "wb") as f:
+        f.write(response["AudioStream"].read())
+
+    note = genanki.Note(model=model, fields=[japanese, english])
     deck.add_note(note)
 
 
 def deck_from_word_list():
     pass
 
-model = genanki.Model(
-  1607392319,
-  'Simple Model',
-  fields=[
-    {'name': 'Question'},
-    {'name': 'Answer'},
-  ],
-  templates=[
-    {
-      'name': 'Card 1',
-      'qfmt': '{{Question}}',
-      'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
-    },
-  ])
 
-deck = genanki.Deck( 2059400110, "Example words")
+model = genanki.Model(
+    1607392319,
+    "Simple Model",
+    fields=[
+        {"name": "Question"},
+        {"name": "Answer"},
+        {"name": "MyMedia"},
+    ],
+    templates=[
+        {
+            "name": "Card 1",
+            "qfmt": "{{Question}}",
+            "afmt": '{{FrontSide}}<hr id="answer">{{Answer}}{{MyMedia}}',
+        },
+    ],
+)
+
+deck = genanki.Deck(2059400110, "Example words")
 
 add_card(deck, model, "assistant")
 
-genanki.Package(deck).write_to_file('output.apkg')
-
+pkg = genanki.Package(deck)
+# pkg.media_files = ... TODO
+pkg.write_to_file("output.apkg")
